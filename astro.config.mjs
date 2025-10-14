329bd48ba68c9ae905380b3ecba3deba5c830ebe
Zach Knickerbocker <zknicker@gmail.com>
2025-10-14 15:32:06 -0400

    refactor: migrate site to astro and yarn

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  srcDir: './src',
+  typescript: {
+    strict: true,
+  },
+  alias: {
+    '@': './src',
+  },
+})
