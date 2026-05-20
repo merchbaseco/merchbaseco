e02ac8acc453773cfd3dab63df69150b050da630
Zach Knickerbocker <zknicker@gmail.com>
2026-05-20 16:23:37 -0400

    fix: upgrade merchbase site astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
 
 const site = process.env.PUBLIC_SITE_URL ?? "https://merchbase.co";
 const base = process.env.PUBLIC_BASE_PATH ?? "/";
@@ -8,7 +7,7 @@ const base = process.env.PUBLIC_BASE_PATH ?? "/";
 export default defineConfig({
   site,
   base,
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   srcDir: "./src",
   typescript: {
     strict: true,
